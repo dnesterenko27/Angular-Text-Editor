a9309532dd77b8d3daba11e9780783a871440b69
Danil Nesterenko <d.nesterenko@nodeart.io>
2020-01-11 19:40:22 +0200

    Initiation

--- src/app/control-panel/control-panel.component.ts
+++ src/app/control-panel/control-panel.component.ts
@@ -1,10 +1,31 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostListener, Input } from '@angular/core';
 
 @Component({
   selector: 'app-control-panel',
   templateUrl: './control-panel.component.html',
   styleUrls: ['./control-panel.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ControlPanelComponent {
+  @Input() el: HTMLElement;
+
+  @HostListener('click', ['$event']) onClick($event) {
+    $event.stopPropagation();
+  }
+
+  toggleBold(el: HTMLElement) {
+    el.style.fontWeight = el.style.fontWeight === 'bold' ? 'normal' : 'bold';
+  }
+
+  toggleItalic(el: HTMLElement) {
+    el.style.fontStyle = el.style.fontStyle === 'italic' ? 'normal' : 'italic';
+  }
+
+  toggleUnderline(el: HTMLElement) {
+    el.style.textDecoration = el.style.textDecoration === 'underline' ? 'none' : 'underline';
+  }
+
+  setColor(el: HTMLElement, event) {
+    el.style.color = event.target.value;
+  }
 }
